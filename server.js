faa95cffd4dcc72601bd36b9f49ac50286542f9a
Felipe Quiros <quirosfelipe@gmail.com>
2020-03-30 18:35:13 -0400

    all codes added

--- server.js
+++ server.js
@@ -2,7 +2,39 @@ const http = require('http');
 
 http.createServer(function (req, res) {
     if (req.url === '/OK') {
-        res.writeHead(200);
+        res.writeHead('/200');
+        res.end();
+
+    } else if (req.url === '/Created') {
+        res.writeHead(201);
+        res.end();
+
+    } else if (req.url === '/Moved') {
+        res.writeHead(301);
+        res.end();
+
+    } else if (req.url === '/Found') {
+        res.writeHead(302);
+        res.end();
+
+    } else if (req.url === '/Bad-Request') {
+        res.writeHead(400);
+        res.end();
+
+    } else if (req.url === '/Unauthorzied') {
+        res.writeHead(401);
+        res.end();
+
+    } else if (req.url === '/Forbidden') {
+        res.writeHead(403);
+        res.end();
+
+    } else if (req.url === '/Internal-Server-Error') {
+        res.writeHead(500);
+        res.end();
+
+    } else if (req.url === '/Gateway-Timeout') {
+        res.writeHead(504);
         res.end();
 
     } else {
